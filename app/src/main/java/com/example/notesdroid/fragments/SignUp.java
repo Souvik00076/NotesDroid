35fd78b4062bf8adaf3228aa9e14f8848af0474d
Techno Exponent <Souvik000762@gmail.com>
2023-06-14 13:55:01 +0530

    Complete the todos

--- app/src/main/java/com/example/notesdroid/fragments/SignUp.java
+++ app/src/main/java/com/example/notesdroid/fragments/SignUp.java
@@ -11,7 +11,9 @@ import android.view.ViewGroup;
 import com.example.notesdroid.R;
 
 public class SignUp extends Fragment {
-
+    //TODO 1 : DEFINE THE XML ELEMENTS HERE.
+    //TODO 2 : DEFINE THE ON CLICK LISTENER INSIDE ONCREATEVIEW.
+    //TODO 3: DEFINE A FUNCTION CALLED VALIDITY CHECK THAT TAKES EMAIL,PASSWORD, AND CONFIRM PASSWORD AND CHECK WHETHER THEY ARE VALID OR NOT
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
