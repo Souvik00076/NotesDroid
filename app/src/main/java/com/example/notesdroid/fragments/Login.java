d956347fbaec4eb2d3f0fdb55cf2f3f9de6015f9
Souvik00076 <souvikbhattacharjee00076@gmail.com>
2023-06-17 14:10:06 +0530

    Complete the todos

--- app/src/main/java/com/example/notesdroid/fragments/Login.java
+++ app/src/main/java/com/example/notesdroid/fragments/Login.java
@@ -2,24 +2,61 @@ package com.example.notesdroid.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.notesdroid.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends Fragment {
-    //TODO -1 Declare all the edit texts and button specified here with fruitful name
+    private EditText emailEt, passwordEt;
+    private Button loginButton;
+    private FirebaseAuth auth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //TODO -2 Inflate the root view
-        // TODO -4 call the init method by passing root view
-
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+        init(root);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEt.getText().toString();
+                String password = passwordEt.getText().toString();
+                if (email.isEmpty()) {
+                    //TODO -1 handle it
+                }
+                if (password.isEmpty()) {
+                    //TODO -2 handle it
+                }
+                //TODO 3 call the loginUser method with email and password
+            }
+        });
         return root;
     }
 
-    //TODO -3 Define a method as init that takes one parameter of view type and define the widgets
+    private void loginUser(final String email, final String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+            }
+        });
+    }
+
+    private void init(View root) {
+        emailEt = root.findViewById(R.id.email_et);
+        passwordEt = root.findViewById(R.id.password_et);
+        loginButton = root.findViewById(R.id.login_button);
+        auth = FirebaseAuth.getInstance();
+    }
 }
